1f354f5f44094cab9cd9907dc7fc8da85310a476
James Cooper <cooper.jm@outlook.com>
2025-05-05 00:46:34 -0400

    base fix

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  bas: '/pixi-project/',
+  base: '/pixi-project/',
   server: {
     port: 8080,
     open: true,
